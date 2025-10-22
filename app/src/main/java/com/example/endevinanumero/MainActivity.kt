2d6fe043ebe7c95e1d9eab78dc757536f8eea108
Jime1805 <18ejimen@gmail.com>
2025-10-23 00:03:12 +0200

    Menu hecho start activity empezada

--- app/src/main/java/com/example/endevinanumero/MainActivity.kt
+++ app/src/main/java/com/example/endevinanumero/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.endevinanumero
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -7,18 +8,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     lateinit var tvFecha : TextView
     lateinit var btnEnviar : Button
+    lateinit var tieMensaje : TextInputEditText
+
+    companion object{
+        const val StartActivity_KEY = "StartActivity"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initComponents()
         initUi()
+        initListeners()
+    }
+
+    private fun initListeners(){
+        btnEnviar.setOnClickListener{
+            val user = tieMensaje.text.toString()
+            navigateToStartActivity(user)
+        }
+    }
+
+    private fun navigateToStartActivity(user: String){
+        val intent = Intent(this, StartActivity::class.java)
+        intent.putExtra(StartActivity_KEY, user)
+        startActivity(intent)
     }
 
     private fun setFecha(){
@@ -36,5 +58,6 @@ class MainActivity : AppCompatActivity() {
     private fun initComponents(){
         tvFecha = findViewById(R.id.tvFecha)
         btnEnviar = findViewById(R.id.btnEnviar)
+        tieMensaje = findViewById(R.id.tieMensaje)
     }
 }
--- app/src/main/java/com/example/endevinanumero/StartActivity.kt
+++ app/src/main/java/com/example/endevinanumero/StartActivity.kt
@@ -1,14 +1,26 @@
 package com.example.endevinanumero
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.endevinanumero.MainActivity.Companion.StartActivity_KEY
 
 class StartActivity : AppCompatActivity() {
+
+    lateinit var tvTittle_start: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val user: String = intent.extras?.getString(StartActivity_KEY) ?: "invitado"
+        initUi(user)
     }
+
+    private fun initUi(user: String){
+        tvTittle_start.text = "Benvingut: $user"
+    }
+
 }
